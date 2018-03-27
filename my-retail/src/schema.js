4298d463db896c951d5b58a8102057cba2672d87
Venkatachalapatee Shanmuga Sundaram <Venkatachalapatee@gmail.com>
2018-03-27 18:27:40 -0500

    Saving the work

--- my-retail/src/schema.js
+++ my-retail/src/schema.js
@@ -1,25 +1,93 @@
 const graphql = require('graphql');
+const fetch = require('node-fetch');
 
-const ProductBasicInformationType = graphql.GraphQLObjectType({});
+const ProductBasicInformationType = new graphql.GraphQLObjectType({
+    name: 'ProductBasicInformation',
+    description: '',
+    fields: () => ({
+        name: {
+            type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+        }
+    })
+});
 
-const root = graphql.GraphQLObjectType({
+const ProductPriceInformationType = new graphql.GraphQLObjectType({
+    name: 'ProductPriceInformation',
+    description: '',
+    fields: () => ({
+        id: {
+            type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+        },
+        current_price: {
+            type: new graphql.GraphQLObjectType({
+                name: 'CurrentPrice',
+                description: '',
+                fields: () => ({
+                    value: {
+                        type: new graphql.GraphQLNonNull(graphql.GraphQLFloat)
+                    },
+                    currency_code: {
+                        type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+                    }
+                })
+            })
+        }
+    })
+});
+
+const ProductInformationType = new graphql.GraphQLObjectType({
+   name:'ProductInformation',
+   description:'',
+    fields:()=>({
+        productBasicInformation:{
+            type:ProductBasicInformationType
+        },
+        productPriceInformation:{
+            type:ProductPriceInformationType
+        }
+    })
+});
+const root = new graphql.GraphQLObjectType({
     name: 'Root',
     description: '',
     fields: () => ({
         productBasicInformation: {
             type: ProductBasicInformationType,
-            args:{
-                id:{
+            args: {
+                id: {
                     type: new graphql.GraphQLNonNull(graphql.GraphQLString)
                 }
             },
-            resolve: {
-
-            }
-
-
+            resolve: (root, args) =>
+                fetch(`https://redsky.target.com/v2/pdp/tcin/${args.id}`)
+                    .then(res => res.json())
+                    .then(json => json)
+        },
+        productPriceInformation: {
+            type: ProductPriceInformationType,
+            args: {
+                id: {
+                    type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+                }
+            },
+            resolve: (root, args) =>
+                fetch(`https://redsky.target.com/v2/pdp/tcin/${args.id}`)
+                    .then(res => res.json())
+                    .then(json => json)
+        },
+        productInformation:{
+            type: ProductInformationType,
+            args: {
+                id: {
+                    type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+                }
+            },
         }
     })
 
 
+});
+
+module.exports = new graphql.GraphQLSchema({
+    query: root
 });
